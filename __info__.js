8d70e5bdd8bc0e41dcc7102651b9ed5360ac11ce
rimpa kasundi <rimpakasundi@gmail.com>
2025-10-25 20:58:33 +0530

    Add Register component with responsive form design

---
+++ __info__.js
@@ -0,0 +1,8 @@
+/**
+ * 1. First time visit. (No account before)
+ *    sign up == create account == register
+ *
+ *  2. Already have an account .(Second time / account having after )
+ *  sign in === log in
+ *
+ */
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -2,8 +2,32 @@ import React from "react";
 
 const Register = () => {
   return (
-    <div>
-      <p>i am register page</p>
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Register now!</h1>
+        </div>
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <div className="card-body">
+            <form>
+              <fieldset className="fieldset">
+                <label className="label">Email</label>
+                <input type="email" className="input" placeholder="Email" />
+                <label className="label">Password</label>
+                <input
+                  type="password"
+                  className="input"
+                  placeholder="Password"
+                />
+                <div>
+                  <a className="link link-hover">Forgot password?</a>
+                </div>
+                <button className="btn btn-neutral mt-4">Register</button>
+              </fieldset>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
