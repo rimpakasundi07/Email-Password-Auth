16ed6c7c46c3a2e98b87fd16cc311f48ff2590bd
rimpa kasundi <rimpakasundi@gmail.com>
2025-10-26 19:47:52 +0530

    Add login component with form and styling

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,8 +2,24 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div>
-      <p>I am log in page</p>
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <div className="card-body">
+            <h1 className="text-3xl font-bold">Login now!</h1>
+            <fieldset className="fieldset">
+              <label className="label">Email</label>
+              <input type="email" className="input" placeholder="Email" />
+              <label className="label">Password</label>
+              <input type="password" className="input" placeholder="Password" />
+              <div>
+                <a className="link link-hover">Forgot password?</a>
+              </div>
+              <button className="btn btn-neutral mt-4">Login</button>
+            </fieldset>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
